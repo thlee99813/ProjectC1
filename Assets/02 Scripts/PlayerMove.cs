c8eda868044f38245ba0f2e3b43053a4b3c97f02
thlee99813@gmail.com <thlee99813@gmail.com>
2026-03-13 21:33:33 +0900

    [이태환] 플레이어 움직임 및 회전 완료 (우클릭 이동 포함)

--- Assets/02 Scripts/PlayerMove.cs
+++ Assets/02 Scripts/PlayerMove.cs
@@ -4,18 +4,51 @@ public class PlayerMove : MonoBehaviour
 {
     [Header("Move")]
     [SerializeField] private float movespeed = 6f;
+    [SerializeField] private float mouseSensitivity = 100f;
 
     private Rigidbody rb;
     private Vector3 inputMove;
+    private Vector3 targetPos;
+    private bool isMoving;
     
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
     void Update()
     {
         GetInput();
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        Plane ground = new Plane(Vector3.up, transform.position);
+
+        if (ground.Raycast(ray, out float distance))
+        {
+            Vector3 hitPoint = ray.GetPoint(distance);
+            Vector3 dir = hitPoint - transform.position;
+            dir.y = 0f;
+
+            if (dir.sqrMagnitude > 0.01f)
+            {
+                transform.rotation = Quaternion.LookRotation(dir);
+            }
+            if (Mouse.current.rightButton.wasPressedThisFrame)
+            {
+
+                    targetPos = ray.GetPoint(distance);
+                    targetPos.y = rb.position.y;
+                    isMoving = true;
+            }
+        }
+        
+
     }
 
 
@@ -23,6 +56,22 @@ public class PlayerMove : MonoBehaviour
     {
         Vector3 pos = rb.position + inputMove * movespeed * Time.fixedDeltaTime;
         rb.MovePosition(pos);
+        if (isMoving)
+        {
+            Vector3 dir = targetPos - rb.position;
+            dir.y = 0f;
+
+            if (dir.sqrMagnitude < 0.05f)
+            {
+                isMoving = false;
+                return;
+            }
+
+            dir = dir.normalized;
+
+            Vector3 nextPos = rb.position + dir * movespeed * Time.fixedDeltaTime;
+            rb.MovePosition(nextPos);
+        }
     }
 
     private void GetInput()
@@ -36,4 +85,5 @@ public class PlayerMove : MonoBehaviour
         move = move.normalized;
         inputMove = new Vector3(move.x, 0f, move.y);
     }
+
 }
